c878ba35c1a18c414693dc930720c193fa9f6bcf
agat-prog <agatalba@gmail.com>
2023-02-03 15:53:48 +0100

    Correccón

--- docs-api/src/main/java/mca/filesmanagement/docs/DocsApp.java
+++ docs-api/src/main/java/mca/filesmanagement/docs/DocsApp.java
@@ -1,16 +1,16 @@
 package mca.filesmanagement.docs;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Clase principal para el inicio y configuración de la aplicación de documentos.
  * @author agat
  */
 @SpringBootApplication
-public final class DocsApp {
-
-	private DocsApp() {}
+public class DocsApp {
 
 	/**
 	 * Entrada principal de la aplicación de documentos.
@@ -18,5 +18,14 @@ public final class DocsApp {
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(DocsApp.class, args);
+	}
+
+	/**
+	 * Devuelve el mapeador de objetos a utilizar.
+	 * @return ModelMapper
+	 */
+	@Bean
+	public ModelMapper modelMapper() {
+	    return new ModelMapper();
 	}
 }
